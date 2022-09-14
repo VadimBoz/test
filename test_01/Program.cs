f31b7b5c5da96576e6bbc5aa24ee8ec44392deb7
vadim <vadim3@mail.ru>
2022-09-14 17:43:46 +0300

    add programm

--- test_01/Program.cs
+++ test_01/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] stringArray = new string[] { "hellow", "2", "world", "123" , "1234", "21"};
+Console.WriteLine("Исходный массив");
+
+PrintArray(stringArray);
+string[] res = ResizeArray(stringArray);
+Console.WriteLine("Результирующий массив");
+PrintArray(res);
+
+string[] ResizeArray(string[] array) //  новый массив с эл-ми до 3 символов ------------------------
+{
+    int length = array.Length;
+    string[] tempArray = new string[length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            tempArray[j] = array[i];
+            j++;
+        }
+    }
+    string[] resArray = new string[j];
+
+    for (int i = 0; i < j; i++)
+    {
+        resArray[i] = tempArray[i];
+    }
+    return resArray;
+}
+
+
+void PrintArray(string[] array) //  вывод массива на консоль ----------------------------
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+
+}
